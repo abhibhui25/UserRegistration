df51c554b4649bc9777439a2e7491b077c146a39
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-10-03 21:50:33 +0530

    welcome

---
+++ UserRegistration/src/com/cg/userregistration/UserRegistration.java
@@ -0,0 +1,15 @@
+package com.cg.userregistration;
+
+import java.util.regex.*;
+import java.util.*;
+
+/**
+ * @author Abhishek Bhui
+ *
+ */
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to user registration");
+	}
+}
