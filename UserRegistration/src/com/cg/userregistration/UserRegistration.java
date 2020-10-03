fdad92e7882a6d127a578823fea25b10c3e11ddd
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-10-03 21:54:46 +0530

    added a method to validate first name

--- UserRegistration/src/com/cg/userregistration/UserRegistration.java
+++ UserRegistration/src/com/cg/userregistration/UserRegistration.java
@@ -7,9 +7,21 @@ import java.util.*;
  * @author Abhishek Bhui
  *
  */
+
 public class UserRegistration {
+	/**
+	 * * UC1-Validate first name
+	 */
+	public static boolean firstName() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Name: ");
+		String first = sc.next();
+		boolean result = Pattern.matches("^[A-Z][aA-zZ]{2,}", first);
+		return result;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration");
+		System.out.println(firstName());
 	}
 }
