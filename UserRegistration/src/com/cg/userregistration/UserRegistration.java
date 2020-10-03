434e71fc906a0328c05e7ac06e8014f54e9b73eb
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-10-03 22:01:49 +0530

    added a method to validate last name

--- UserRegistration/src/com/cg/userregistration/UserRegistration.java
+++ UserRegistration/src/com/cg/userregistration/UserRegistration.java
@@ -20,8 +20,27 @@ public class UserRegistration {
 		return result;
 	}
 
+	/**
+	 * UC2-Validate last name
+	 */
+	public static boolean lastName() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Last Name: ");
+		String last = sc.next();
+		boolean result = Pattern.matches("^[A-Z][aA-zZ]{2,}", last);
+		return result;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to user registration");
-		System.out.println(firstName());
+		if(firstName())
+			System.out.println("valid");
+		else
+			System.out.println("invalid");
+		if(lastName())
+			System.out.println("valid");
+		else
+			System.out.println("invalid");
+				
 	}
 }
