ffa1a56c4a57d498a68714d63fe3a364116e810a
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-10-05 20:48:27 +0530

    added a method to validate email

---
+++ UserRegistration/src/com/cg/userregistration/UserRegistration.java
@@ -0,0 +1,63 @@
+package com.cg.userregistration;
+
+import java.util.regex.*;
+import java.util.*;
+
+/**
+ * @author Abhishek Bhui
+ *
+ */
+
+public class UserRegistration {
+	/**
+	 * * UC1-Validate first name
+	 */
+	public static boolean firstName() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Name: ");
+		String first = sc.next();
+		boolean result = Pattern.matches("^[A-Z][aA-zZ]{2,}", first);
+		return result;
+	}
+
+	/**
+	 * UC2-Validate last name
+	 */
+	public static boolean lastName() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Last Name: ");
+		String last = sc.next();
+		boolean result = Pattern.matches("^[A-Z][aA-zZ]{2,}", last);
+		return result;
+	}
+
+	/**
+	 * UC3-Validate email
+	 */
+	public static boolean email() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter email: ");
+		String email = sc.next();
+		boolean result = Pattern.matches(
+				"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9]+)*@" + "[_A-Za-z0-9-]+(\\.[_A-Za-z0-9]+){0,1}(\\.[A-Za-z]{2,})$",
+				email);
+		return result;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to user registration");
+		if (firstName())
+			System.out.println("valid");
+		else
+			System.out.println("invalid");
+		if (lastName())
+			System.out.println("valid");
+		else
+			System.out.println("invalid");
+		if (email())
+			System.out.println("valid");
+		else
+			System.out.println("invalid");
+
+	}
+}
