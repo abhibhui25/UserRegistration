fef33214552e88d5b9ad1aeae558d7694f9302ed
Abhishek Bhui <abhishekbhui11@gmail.com>
2020-10-16 09:37:57 +0530

    added a method to find maximum and test if maximum is at position 1

--- FindMaximum/src/main/java/com/cg/findmaximum/FindMaximum.java
+++ FindMaximum/src/main/java/com/cg/findmaximum/FindMaximum.java
@@ -1,7 +1,17 @@
 package com.cg.findmaximum;
 
 public class FindMaximum {
+    public Integer maxInteger(Integer a,Integer b,Integer c){
+        if(a.compareTo(b)>0 && a.compareTo(c)>0)
+            return a;
+        if(b.compareTo(a)>0 && b.compareTo(c)>0)
+            return b;
+        else
+            return c;
+    }
+
     public static void main(String[] args) {
         System.out.println("welcome");
+        FindMaximum max=new FindMaximum();
     }
 }
---
+++ FindMaximum/src/test/java/com/cg/findmaximum/TestMaximum.java
@@ -0,0 +1,28 @@
+package com.cg.findmaximum;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestMaximum {
+    FindMaximum m=new FindMaximum();
+    Integer a,b,c;
+    Float d,e,f;
+    String g,h,i;
+
+    @Before
+    public void initialValues(){
+        a=10;
+        b=5;
+        c=2;
+        d=5.0f;
+        e=3.0f;
+        f=1.0f;
+        g="Abhishek";
+        h="Bhui";
+        i="blr";
+    }
+    @Test
+    public void givenIntegers_IfMaxAtPosition1_ShouldReturnTrue(){
+        Assert.assertSame(m.maxInteger(a,b,c),a);
+    }
+}
